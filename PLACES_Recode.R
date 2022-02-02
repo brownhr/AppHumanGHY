c489c5cf629bc9d3cc2f11292eed36e2cde28297
brownhr <brownhr314@gmail.com>
2022-02-02 14:54:02 -0500

    Initialized R Project; Created scripts
    Working on #3:
    - [ ] Get ZCTA for Western NC
    - [ ] Filter unnecessary columns
    - [ ] Recode PLACES

---
+++ PLACES_Recode.R
@@ -0,0 +1,10 @@
+library(readr)
+library(dplyr)
+
+
+Pop_places <- read_csv("Pop_Places.csv")
+
+pop_select <- Pop_places %>%
+  dplyr::filter(
+    
+  )
---
+++ get_ZCTA.R
@@ -0,0 +1,11 @@
+library(tigris)
+library(sf)
+library(dplyr)
+
+# Get ZCTA for Western NC 
+
+
+  # Find counties in WNC
+
+
+  # Intersect ZCTA with sf
