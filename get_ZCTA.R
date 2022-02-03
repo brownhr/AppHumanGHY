55cfbe29ee60e1ebce39f9fe68f116a23d18c5c0
brownhr <brownhr314@gmail.com>
2022-02-03 10:26:42 -0500

    Get shapefile of counties for WNC #3

--- get_ZCTA.R
+++ get_ZCTA.R
@@ -2,10 +2,60 @@ library(tigris)
 library(sf)
 library(dplyr)
 
-# Get ZCTA for Western NC 
+# Get ZCTA for Western NC
 
+# Simple list of the counties within WNC [@wnc_state_geography]
+wnc_counties_list <- c(
+  "Alleghany",
+  "Ashe",
+  "Avery",
+  "Buncombe",
+  "Burke",
+  "Caldwell",
+  "Cherokee",
+  "Clay",
+  "Graham",
+  "Haywood",
+  "Henderson",
+  "Jackson",
+  "Macon",
+  "Madison",
+  "McDowell",
+  "Mitchell",
+  "Polk",
+  "Rutherford",
+  "Swain",
+  "Transylvania",
+  "Watauga",
+  "Wilkes",
+  "Yancey"
+)
 
-  # Find counties in WNC
+nc_counties <- tigris::counties(
+  state = "NC",
+  cb = T
+)
+
+# Find counties in WNC
+
+wnc_counties <- nc_counties %>% 
+  # Filter by counties within wnc_counties_list
+  dplyr::filter(
+    NAME %in% wnc_counties_list
+  ) %>% 
+  # Most of the data is unnecessary; just pull out
+  # whatever we need (NAME, GEOID, and geometry)
+  dplyr::select(
+    GEOID, NAME, geometry
+  )
+
+# Save the results to a file so we can use this later
+
+st_write(
+  obj = wnc_counties,
+  dsn = "data/shp/wnc_counties.shp"
+)
+
 
 
   # Intersect ZCTA with sf
