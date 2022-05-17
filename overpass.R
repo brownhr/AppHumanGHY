1ab12534512c1fd8117ace27a3fa5fa0e8783691
brownhr <brownhr314@gmail.com>
2022-05-17 11:37:31 -0400

    Update overpass.R

--- overpass.R
+++ overpass.R
@@ -1,4 +1,5 @@
-library(overpass)
+
+library(osmdata)
 library(tidyverse)
 library(sf)
 shp <- tigris::counties(state = "NC", cb = T)
@@ -6,6 +7,30 @@ shp <- tigris::counties(state = "NC", cb = T)
 watauga <- filter(shp, NAME == "Watauga")
 
 bbox <- st_bbox(watauga)
-query <- paste0('[out:json];(node["amenity"](', cat(as.character(bbox), sep = ","), '););(._;>;);out;')
 
-wat_amenities <- overpass_query(query)
+q <- opq(
+  bbox = unname(bbox)
+) %>% 
+  add_osm_feature(
+    key = "amenity"
+  ) %>% 
+  osmdata_sf()
+
+points <- q$osm_points
+
+points2 <- points %>% 
+  head(n = 200) %>%
+  select(-starts_with("addr."),
+         -starts_with("contact"),
+         -starts_with("gnis"),
+         -amenity,
+         -area,
+         -description,
+         -ele,
+         -religion
+         ) %>% 
+  pivot_longer(
+    cols = -c(osm_id, geometry, name),
+    names_to = "amenity_type",
+    values_drop_na = T,
+    values_to = "key")
