af22b18002321988968dae3fa256e4543d49424e
brownhr <brownhr314@gmail.com>
2022-05-17 16:19:59 -0400

    Update overpass.R

--- overpass.R
+++ overpass.R
@@ -1,36 +1,40 @@
-
+# library(tmap)
 library(osmdata)
 library(tidyverse)
 library(sf)
-shp <- tigris::counties(state = "NC", cb = T)
 
-watauga <- filter(shp, NAME == "Watauga")
+# wnc_counties <- read_sf("data/shp/wnc_counties.shp") %>% 
+#   st_transform(crs = st_crs("EPSG:4326"))
+# 
+# wnc_bbox <- st_bbox(wnc_counties)
+# 
+# get_amenities <- function(bbox) {
+#   q <- opq(bbox = unname(bbox)) %>%
+#     add_osm_feature(key = "amenity") %>%
+#     osmdata_sf() %>%
+#     unique_osmdata()
+# }
+# 
+# get_centroids <- function(shp) {
+#   centroids <- shp[4:7] %>%
+#     map(~ mutate(.x,
+#       geometry = st_centroid(st_make_valid(geometry))
+#     )) %>%
+#     bind_rows() %>%
+#     select(osm_id, name, amenity, geometry)
+#   return(centroids)
+# }
+# 
+# wnc_amenities <- wnc_bbox %>% 
+#   get_amenities()
+#   get_centroids()
+# 
+# 
+# wnc_amenities_sj <- wnc_amenities %>% 
+#   select(osm_id, name, amenity, geometry) %>% 
+#   st_filter(wnc_counties)
+# 
+# write_sf(wnc_amenities_sj, "data/shp/wnc_amenities.shp")
 
-bbox <- st_bbox(watauga)
 
-q <- opq(
-  bbox = unname(bbox)
-) %>% 
-  add_osm_feature(
-    key = "amenity"
-  ) %>% 
-  osmdata_sf()
-
-points <- q$osm_points
-
-points2 <- points %>% 
-  head(n = 200) %>%
-  select(-starts_with("addr."),
-         -starts_with("contact"),
-         -starts_with("gnis"),
-         -amenity,
-         -area,
-         -description,
-         -ele,
-         -religion
-         ) %>% 
-  pivot_longer(
-    cols = -c(osm_id, geometry, name),
-    names_to = "amenity_type",
-    values_drop_na = T,
-    values_to = "key")
+wnc_amenities <- read_sf("data/shp/wnc_amenities.shp")
