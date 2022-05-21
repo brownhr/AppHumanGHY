d5a9c34a051146703ce1d4ca03d8e8b6fc3a4e54
brownhr <brownhr314@gmail.com>
2022-05-21 17:11:17 -0400

    Update PLACES_Recode.R

--- PLACES_Recode.R
+++ PLACES_Recode.R
@@ -1,10 +1,19 @@
-library(readr)
-library(dplyr)
+library(tidyverse)
+library(sf)
+library(tmap)
 
 
-Pop_places <- read_csv("Pop_Places.csv")
+places_zcta_raw <- read_sf("data/shp/places_wnc_zcta.shp") %>% 
+  st_transform("EPSG:32119") %>% 
+  st_simplify(dTolerance = 100)
 
-pop_select <- Pop_places %>%
-  dplyr::filter(
-    
+
+
+places_zcta_vars <- places_zcta_raw %>% 
+  select(
+    GEOID, ZCTA, TOTPOP,
+    MHLTH, DEPRESSION, SLEEP,
+    geometry
   )
+
+write_sf(places_zcta_vars, "data/shp/places_wnc_zcta.shp")
