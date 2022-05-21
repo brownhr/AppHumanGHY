50b687096eeab1749ee67d94c588c48b8b883959
brownhr <brownhr314@gmail.com>
2022-05-21 17:23:19 -0400

    Rename GS_Shp > GS_zip

---
+++ GS_zip.R
@@ -0,0 +1,24 @@
+library(googledrive)
+library(tidyverse)
+library(sf)
+library(fs)
+
+SEG_dir <- drive_get(id = "18Fp3JptsAwJ3aXavoFOvtJWNbfJhZ0lV")
+GS_zip <- drive_ls(SEG_dir) %>%
+  filter(name == "GS_Shapefile.zip")
+
+gs_dir <- fs::path("data", "shp", "gs")
+
+if (!dir.exists(gs_dir)) {
+  gs_name <- "GS_zip.zip"
+  dir.create(gs_dir)
+  
+  drive_download(GS_zip, path = fs::path(gs_dir, gs_name))
+  
+  unzip(
+    zipfile = fs::path(gs_dir, gs_name),
+    overwrite = TRUE,
+    exdir = gs_dir
+  )
+  file_delete(path(gs_dir, gs_name))
+}
