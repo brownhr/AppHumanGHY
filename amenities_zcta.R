9ebd1beaa95602ff184e72ff49c25224def66cb2
brownhr <brownhr314@gmail.com>
2022-05-21 17:11:04 -0400

    Update amenities_zcta.R

--- amenities_zcta.R
+++ amenities_zcta.R
@@ -2,49 +2,72 @@ library(tidyverse)
 library(sf)
 library(tmap)
 
-crs <- st_crs("EPSG:4326")
+crs <- st_crs("EPSG:32119")
 
-wnc_zcta <- read_sf("data/shp/wnc_zcta.shp") %>% 
+wnc_zcta <- read_sf("data/shp/places_wnc_zcta.shp") %>% 
   st_transform(crs = crs)
 wnc_amenities <- read_sf("data/shp/wnc_amenities.shp") %>% 
   st_transform(crs = crs) %>% 
   mutate(amenity = amenity %>% str_replace_all("_", " ") %>% str_to_title() %>% as.factor())
 
-top_amens <- wnc_amenities %>% 
-  st_drop_geometry() %>% 
-  group_by(amenity) %>% 
-  summarize(
-    count = n()
-  )%>% 
-  ungroup() %>% 
-  na.omit() %>% 
-  arrange(desc(count))
+zcta_geom <- wnc_zcta %>% 
+  select(ZCTA, geometry)
 
-top_20_amens <- top_amens  %>% 
-  slice_head(n = 20) %>% 
-  mutate(
-    amenity = fct_reorder(amenity, count)
+
+wnc_amenities_join <- wnc_amenities %>% 
+  st_join(
+    wnc_zcta,
+    left = FALSE
   )
-top_20_amens %>% 
-    ggplot(aes(y = amenity, x = count, fill = amenity)) + 
-  geom_col(show.legend = FALSE) +
-  geom_text(aes(label = count), nudge_x = 150) +
-  labs(
-    x = "Count",
-    y = "Amenity",
-    title = "Top 20 Amenities in Western NC"
-  ) + 
-  theme_light()
 
-ggsave("top_amenities.png")
+wnc_zcta_amenities <- wnc_amenities_join %>% 
+  st_drop_geometry() %>%
+  select(ZCTA, amenity) %>% 
+  group_by(ZCTA) %>% 
+  count(amenity) %>% 
+    ungroup() %>% 
+  pivot_wider(names_from = amenity, values_from = n, values_fill = 0)
 
-wnc_amenities %>% 
-  filter(amenity %in% top_20_amens$amenity) %>%
-  mutate(
-    amenity = fct_infreq(amenity)
-  ) %>% 
-  ggplot(aes(color = amenity)) + 
-  geom_sf(show.legend = TRUE) + 
-  theme_light()
+wnc_zcta_amenities <- zcta_geom %>% 
+  right_join(wnc_zcta_amenities)
 
-ggsave("top_amens_map.png")
+
+
+# top_amens <- wnc_amenities %>% 
+#   st_drop_geometry() %>% 
+#   group_by(amenity) %>% 
+#   summarize(
+#     count = n()
+#   )%>% 
+#   ungroup() %>% 
+#   na.omit() %>% 
+#   arrange(desc(count))
+# 
+# top_20_amens <- top_amens  %>% 
+#   slice_head(n = 20) %>% 
+#   mutate(
+#     amenity = fct_reorder(amenity, count)
+#   )
+# top_20_amens %>% 
+#     ggplot(aes(y = amenity, x = count, fill = amenity)) + 
+#   geom_col(show.legend = FALSE) +
+#   geom_text(aes(label = count), nudge_x = 150) +
+#   labs(
+#     x = "Count",
+#     y = "Amenity",
+#     title = "Top 20 Amenities in Western NC"
+#   ) + 
+#   theme_light()
+# 
+# ggsave("top_amenities.png")
+# 
+# wnc_amenities %>% 
+#   filter(amenity %in% top_20_amens$amenity) %>%
+#   mutate(
+#     amenity = fct_infreq(amenity)
+#   ) %>% 
+#   ggplot(aes(color = amenity)) + 
+#   geom_sf(show.legend = TRUE) + 
+#   theme_light()
+# 
+# ggsave("top_amens_map.png")
