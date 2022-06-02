85f73e9b8882197b935065ec368cd4cf1c83ae78
brownhr <brownhr314@gmail.com>
2022-06-02 15:05:03 -0400

    Update LISA.R

--- R/LISA.R
+++ R/LISA.R
@@ -21,30 +21,60 @@ calc_lisa <- function(data, lisa_vars){
 zcta_main2 <- zcta_main %>% 
   calc_lisa(lisa_vars = c(MHLTH, DEPRESSION, SLEEP, NDVI_MEAN))
 
-tmap_LISA <- function(shp, var) {
-  LISA_palette <- c("Low-Low" = "dodgerblue3", 
-                    "Low-High" = "dodgerblue", 
-                    "High-Low" = "firebrick1", 
-                    "High-High" = "firebrick3")
+tmap_LISA <- function(shp, var, greyscale = FALSE) {
   
+  if (greyscale) {
+    LISA_palette <- c(
+      "Low-Low" = "gray70",
+      "Low-High" = "gray60",
+      "High-Low" = "gray50",
+      "High-High" = "gray40"
+    )
+    na_col <- "gray95"
+  } else{
+    LISA_palette <- c(
+      "Low-Low" = "dodgerblue3",
+      "Low-High" = "dodgerblue",
+      "High-Low" = "firebrick1",
+      "High-High" = "firebrick3"
+    )
+    na_col <- "gray80"
+  }
   
-  
-  t <- tm_shape(shp) + 
-    tm_polygons(col = var, palette = LISA_palette, border.alpha = 0.2, border.col = "white")
+
+
+  t <- tm_shape(shp) +
+    tm_polygons(
+      col = var,
+      palette = LISA_palette,
+      border.alpha = 0.3,
+      border.col = "white",
+      colorNA = na_col,
+      lwd = 1.5
+    )
   
   t
   
 }
 
-LISA_list <- c("MHLTH_LISA", "DEPRESSION_LISA", "SLEEP_LISA", "NDVI_MEAN_LISA") %>% 
-  map(~tmap_LISA(shp = zcta_main2 %>% st_simplify(dTolerance = 50), var = .x))
+LISA_list <-
+  c("MHLTH_LISA",
+    "DEPRESSION_LISA",
+    "SLEEP_LISA",
+    "NDVI_MEAN_LISA") %>%
+  map( ~ tmap_LISA(
+    shp = zcta_main2 %>% st_simplify(dTolerance = 50),
+    var = .x,
+    greyscale = T
+  ))
 
 
 lisa_tmap_list <- tmap_arrange(LISA_list)
+lisa_tmap_list
 
 tmap_save(
   lisa_tmap_list,
-  filename = "fig/multi_LISA.png",
+  filename = "fig/multi_LISA_greyscale.png",
   dpi = 300,
   width = 10,
   height = 7.5
