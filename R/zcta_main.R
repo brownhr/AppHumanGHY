8bd380b0a5d736dc2a443a4a15c54987c0f87976
brownhr <brownhr314@gmail.com>
2022-06-07 16:31:26 -0400

    Update zcta_main.R

--- R/zcta_main.R
+++ R/zcta_main.R
@@ -1,8 +1,21 @@
 zcta_list <- list(
   places_zcta %>% select(-GEOID10),
-  trail_summary %>% st_drop_geometry() %>% select(ZCTA, sum_length),
-  NDVI_summary %>% st_drop_geometry() %>% select(ZCTA, NDVI_MEAN),
-  zcta_pri
+  
+  trail_summary,
+  
+  NDVI_summary %>%
+    st_drop_geometry() %>%
+    select(ZCTA, NDVI_MEAN),
+  
+  gs_area %>% 
+    st_drop_geometry() %>% 
+    rename(
+      greenspace_sqkm = gs_area,
+      tot_area_sqkm = Areasqkm
+    ),
+  
+  zcta_pri %>% 
+    rename(population_acs2018 = population)
 )
 
 
@@ -10,4 +23,8 @@ zcta_main <- purrr::reduce(
   zcta_list,
   dplyr::left_join,
   by = "ZCTA"
-)
+) %>% 
+  replace_na(list(trail_per_area = units::set_units(0, 1/km)))
+
+zcta_main_nogeom <- zcta_main %>% 
+  st_drop_geometry()
