b7a8c1a55ebce6d77ac332618dff3e0b07e101d9
brownhr <brownhr314@gmail.com>
2022-06-07 16:55:36 -0400

    Update zcta_main.R

--- R/zcta_main.R
+++ R/zcta_main.R
@@ -1,18 +1,25 @@
 zcta_list <- list(
   places_zcta %>% select(-GEOID10),
   
-  trail_summary,
+  trail_summary %>% 
+    st_drop_geometry(),
   
   NDVI_summary %>%
     st_drop_geometry() %>%
     select(ZCTA, NDVI_MEAN),
   
-  gs_area %>% 
-    st_drop_geometry() %>% 
+  gs_area  %>% 
     rename(
-      greenspace_sqkm = gs_area,
-      tot_area_sqkm = Areasqkm
-    ),
+      greenspace_sqkm = gs_area
+    ) %>% 
+    mutate(
+      greenspace_sqkm = units::set_units(greenspace_sqkm, km^2),
+      tot_area_sqkm = units::set_units(st_area(geometry), km^2)
+    ) %>% 
+    mutate(
+      greenspace_n = greenspace_sqkm / tot_area_sqkm
+    )%>% 
+    st_drop_geometry(),
   
   zcta_pri %>% 
     rename(population_acs2018 = population)
@@ -25,6 +32,11 @@ zcta_main <- purrr::reduce(
   by = "ZCTA"
 ) %>% 
   replace_na(list(trail_per_area = units::set_units(0, 1/km)))
+
+zcta_simplify <- zcta_main %>% 
+  st_simplify(dTolerance = 50)
+
+
 
 zcta_main_nogeom <- zcta_main %>% 
   st_drop_geometry()
