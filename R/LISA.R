b215fda4b99227ed4e34a2c8593ec612a4817917
brownhr <brownhr314@gmail.com>
2022-06-07 16:31:38 -0400

    Update LISA.R

--- R/LISA.R
+++ R/LISA.R
@@ -1,25 +1,20 @@
-LISA <- localmoran(zcta_main$NDVI_MEAN, listw = zcta_listw)
+library(tmap)
 
-calc_lisa <- function(data, lisa_vars){
-  
-  
-  lisa_val <- function(sp_var){
-    L <- localmoran(x = sp_var,
-                    listw = zcta_listw, zero.policy = T, na.action = na.pass,
-                    alternative = "two.sided")
-    L_quadr <- attr(L, "quadr")[,1]
-    levels(L_quadr) <- c("Low-Low", "Low-High", "High-Low", "High-High")
-    return(L_quadr)
-  }
-  
-  data %>% 
-    mutate(across({{lisa_vars}}, lisa_val, .names = "{.col}_LISA" ))
-  
-  
+
+lisa_val <- function(sp_var) {
+  L <- localmoran(
+    x = sp_var,
+    listw = zcta_listw,
+    zero.policy = T,
+    na.action = na.pass,
+    alternative = "two.sided"
+  )
+  L_quadr <- attr(L, "quadr")[, 1]
+  levels(L_quadr) <-
+    c("Low-Low", "Low-High", "High-Low", "High-High")
+  return(L_quadr)
 }
 
-zcta_main2 <- zcta_main %>% 
-  calc_lisa(lisa_vars = c(MHLTH, DEPRESSION, SLEEP, NDVI_MEAN))
 
 tmap_LISA <- function(shp, var, greyscale = FALSE) {
   
@@ -42,7 +37,6 @@ tmap_LISA <- function(shp, var, greyscale = FALSE) {
   }
   
 
-  var <- deparse(substitute(var))
   t <- tm_shape(shp) +
     tm_polygons(
 
@@ -54,29 +48,34 @@ tmap_LISA <- function(shp, var, greyscale = FALSE) {
       lwd = 1.5
     )
   
-  t
+  return(t)
   
 }
 
-LISA_list <-
-  c("MHLTH_LISA",
-    "DEPRESSION_LISA",
-    "SLEEP_LISA",
-    "NDVI_MEAN_LISA") %>%
-  map( ~ tmap_LISA(
-    shp = zcta_main2 %>% st_simplify(dTolerance = 50),
-    var = .x,
-    greyscale = T
-  ))
 
 
-lisa_tmap_list <- tmap_arrange(LISA_list)
-lisa_tmap_list
 
-tmap_save(
-  lisa_tmap_list,
-  filename = "fig/multi_LISA_greyscale.png",
-  dpi = 300,
-  width = 10,
-  height = 7.5
-)
+
+
+# LISA_list <-
+#   c("MHLTH_LISA",
+#     "DEPRESSION_LISA",
+#     "SLEEP_LISA",
+#     "NDVI_MEAN_LISA") %>%
+#   map( ~ tmap_LISA(
+#     shp = zcta_main2 %>% st_simplify(dTolerance = 50),
+#     var = .x,
+#     greyscale = T
+#   ))
+
+# 
+# lisa_tmap_list <- tmap_arrange(LISA_list)
+# lisa_tmap_list
+# 
+# tmap_save(
+#   lisa_tmap_list,
+#   filename = "fig/multi_LISA_greyscale.png",
+#   dpi = 300,
+#   width = 10,
+#   height = 7.5
+# )
