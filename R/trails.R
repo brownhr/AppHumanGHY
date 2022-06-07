bf4ab01fce77be40004246196c8346f2a88de835
brownhr <brownhr314@gmail.com>
2022-06-07 16:31:32 -0400

    Update trails.R

--- R/trails.R
+++ R/trails.R
@@ -1,6 +1,15 @@
-trails <- st_read("data/shp/trails.shp")
+# trails <- st_read("data/shp/trails.shp")
 
 trail_summary <- st_read("data/shp/trails_summary.shp")
 
-trail_summary <- trail_summary %>% 
-  select()
+trail_summary <- trail_summary  %>%
+  mutate(
+    SUM_Length = units::set_units(SUM_Length, m),
+    Area = st_area(geometry) %>% 
+      units::set_units(km^2)
+  ) %>% 
+  mutate(
+    trail_per_area = SUM_Length / Area
+  ) %>% 
+  st_drop_geometry() %>% 
+  select(ZCTA, trail_per_area)
