824da8ae4f0d63c5856e4ece2001eb7de4d71fdf
brownhr <brownhr314@gmail.com>
2022-06-07 18:07:19 -0400

    Refactored code

--- R/total_amenities.R
+++ R/total_amenities.R
@@ -1,29 +1,19 @@
-{
-  amenities_gs_buffer <-
-    st_read("data/shp/amenities_gs_buffer.shp") %>%
-    st_transform(crs)
-  
-  amens_gs_zcta <- st_join(amenities_gs_buffer %>%
-                             select(name, amenity),
-                           wnc_zcta %>%
-                             select(ZCTA))
-  
-  amens_gs_zcta <- amens_gs_zcta %>%
-    mutate(
-      amenity = amenity %>%
-        stringr::str_replace_all("_", " ") %>%
-        stringr::str_to_title() %>%
-        as.factor
-    )
-  park <- levels(amens_gs_zcta$amenity) %>% str_subset("[Pp]ark")
-  
-  amens_by_zcta <- amens_gs_zcta %>%
-    mutate(amenity = fct_collapse(amenity, Parking = park)) %>%
-    st_drop_geometry() %>%
-    group_by(ZCTA) %>%
-    summarize(n()) %>%
-    mutate(amenities_sum = rowSums(across(-ZCTA))) %>%
-    select(ZCTA, amenities_sum)
-  
-  
-}
+amens_by_zcta <-
+  st_read("data/shp/amenities_gs_buffer.shp") %>%
+  st_transform(crs) %>%
+  select(name, amenity) %>%
+  st_join(wnc_zcta %>%
+    select(ZCTA)) %>%
+  mutate(
+    amenity = amenity %>%
+      stringr::str_replace_all("_", " ") %>%
+      stringr::str_to_title() %>%
+      as.factor()
+  ) %>%
+  mutate(amenity = fct_collapse(amenity, Parking = levels(amenity) %>%
+    str_subset("[Pp]ark"))) %>%
+  st_drop_geometry() %>%
+  group_by(ZCTA) %>%
+  summarize(n()) %>%
+  mutate(amenities_sum = rowSums(across(-ZCTA))) %>%
+  select(ZCTA, amenities_sum)
--- R/zcta_main.R
+++ R/zcta_main.R
@@ -1,42 +1,34 @@
-zcta_list <- list(
+zcta_main <- list(
   places_zcta %>% select(-GEOID10),
-  
-  trail_summary %>% 
+  trail_summary %>%
     st_drop_geometry(),
-  
   NDVI_summary %>%
     st_drop_geometry() %>%
     select(ZCTA, NDVI_MEAN),
-  
-  gs_area  %>% 
-    rename(
-      greenspace_sqkm = gs_area
-    ) %>% 
+  gs_area %>%
+    rename(greenspace_sqkm = gs_area) %>%
     mutate(
       greenspace_sqkm = units::set_units(greenspace_sqkm, km^2),
       tot_area_sqkm = units::set_units(st_area(geometry), km^2)
-    ) %>% 
-    mutate(
-      greenspace_n = greenspace_sqkm / tot_area_sqkm
-    )%>% 
+    ) %>%
+    mutate(greenspace_n = greenspace_sqkm / tot_area_sqkm) %>%
     st_drop_geometry(),
-  
-  zcta_pri %>% 
-    rename(population_acs2018 = population)
-)
+  zcta_pri %>%
+    rename(population_acs2018 = population),
+  amens_by_zcta
+) %>%
+  purrr::reduce(dplyr::left_join,
+    by = "ZCTA"
+  ) %>%
+  replace_na(list(
+    trail_per_area = units::set_units(0, 1 / km),
+    amenities_sum = 0
+  ))
 
-
-zcta_main <- purrr::reduce(
-  zcta_list,
-  dplyr::left_join,
-  by = "ZCTA"
-) %>% 
-  replace_na(list(trail_per_area = units::set_units(0, 1/km)))
-
-zcta_simplify <- zcta_main %>% 
+zcta_simplify <- zcta_main %>%
   st_simplify(dTolerance = 50)
 
 
 
-zcta_main_nogeom <- zcta_main %>% 
+zcta_main_nogeom <- zcta_main %>%
   st_drop_geometry()
