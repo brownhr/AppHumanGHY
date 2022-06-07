17faa1a5a5a4b398fede7294f76aec6c0420f9d4
brownhr <brownhr314@gmail.com>
2022-06-07 14:07:32 -0400

    Update zcta_main.R

--- R/zcta_main.R
+++ R/zcta_main.R
@@ -1,20 +1,13 @@
-zcta_main <-
-  left_join(
-    places_zcta,
-    trail_summary %>% st_drop_geometry() %>% select(ZCTA, sum_length),
-    by = c("ZCTA" = "ZCTA")
-  ) %>% 
-  rename(trail_sum = sum_length) #%>% 
-  # replace_na(list(trail_sum = 0))
+zcta_list <- list(
+  places_zcta %>% select(-GEOID10),
+  trail_summary %>% st_drop_geometry() %>% select(ZCTA, sum_length),
+  NDVI_summary %>% st_drop_geometry() %>% select(ZCTA, NDVI_MEAN),
+  zcta_pri
+)
 
 
-
-zcta_main <- zcta_main %>% 
-  mutate(
-    area = st_area(zcta_main) %>% 
-      set_units(km^2),
-    trail_sum = set_units(trail_sum, m) %>% 
-      set_units(km)
-  ) %>% 
-  left_join(NDVI_summary %>% st_drop_geometry() %>% select(ZCTA, NDVI_MEAN))
-
+zcta_main <- purrr::reduce(
+  zcta_list,
+  dplyr::left_join,
+  by = "ZCTA"
+)
