91e816b4c7b9778090178f913cefe73e8225e1b6
brownhr <brownhr314@gmail.com>
2022-06-08 18:13:07 -0400

    Fig

--- .Rprofile
+++ .Rprofile
@@ -2,6 +2,8 @@ source("renv/activate.R")
 
 cat("Run `startup()` to load all the necessary datasets")
 
+Sys.setenv(CHROMOTE_CHROME = "C:\\Users\\brownhr\\AppData\\Local\\Programs\\Opera\\opera.exe")
+
 startup <- function(){
   source("R/startup.R")
 }
--- R/tables.R
+++ R/tables.R
@@ -1,44 +1,38 @@
 library(knitr)
+library(spatialreg)
 library(stargazer)
-library(readr)
+library(tidyverse)
 
 mhealth_lag <- read_rds("data/rds/mhealth.Rds")
 depress_lag <- read_rds("data/rds/depress.Rds")
 sleep_lag <- read_rds("data/rds/sleep.Rds")
 
-
-
-stargazer_to_png <- function(input, dir) {
-  name <- deparse(substitute(input))
-  file <- fs::path(dir, name)
-  file_html <- fs::path_ext_set(file, ".html")
-  file_png <- fs::path_ext_set(file, ".png")
-  
-  out <- capture.output(
-    stargazer::stargazer(input, type = "html", header = FALSE )
-  )
-  cat(paste(out, collapse = "\n"), "\n", file = file_html)
-  webshot::webshot(
-    url = file_html,
-    file = file_png
-  )
-}
-library(tidyverse)
 list(mhealth_lag, depress_lag, sleep_lag) %>% 
   purrr::iwalk(
     function(x, .y){
       t <- summary(x)
       temp <- tempfile(fileext = ".html")
+      # temp <- "fig/out.html"
       stargazer::stargazer(
         t, type = "html", header = FALSE,
         out = temp)
       name <- .y
       
       
-      webshot::webshot(url = fs::path(temp),
+      webshot2::webshot(url = fs::path(temp),
                        file = paste0("fig/",name, ".png"),
                        selector = "tbody")
       fs::file_delete(temp)
     }
   )
   
+t <- tempfile(fileext = ".html")
+stargazer::stargazer(mhealth_lag,
+                     depress_lag,
+                     sleep_lag,
+                     header = F,
+                     type = "html",
+                     out = t, )
+
+webshot2::webshot(t, "fig/table.pdf", selector = "tbody",
+                  expand = 10)
