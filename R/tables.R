101106e54949bdf93e3b33f6414c7b4bd9a24671
brownhr <brownhr314@gmail.com>
2022-06-09 16:59:16 -0400

    Regression Tables

--- R/tables.R
+++ R/tables.R
@@ -26,13 +26,45 @@ list(mhealth_lag, depress_lag, sleep_lag) %>%
     }
   )
   
-t <- tempfile(fileext = ".html")
-stargazer::stargazer(mhealth_lag,
-                     depress_lag,
-                     sleep_lag,
-                     header = F,
-                     type = "html",
-                     out = t, )
 
-webshot2::webshot(t, "fig/table.pdf", selector = "tbody",
-                  expand = 10)
+
+model_list <- list(mhealth_lag, depress_lag, sleep_lag)
+# 
+# model_summary <- model_list %>% 
+#   map(
+# function(x)coef(summary(x))[,c(1,4)])
+
+# t <- tempfile(fileext = ".html")
+# t <- "fig/results_table.html"
+# stargazer::stargazer(
+#   model_list,
+#   header = F,
+#   type = "html",
+#   out = t,
+#   flip = T
+# )
+# 
+# webshot2::webshot(t, "fig/table.pdf", selector = "tbody",
+#                   expand = 10)
+
+library(broom)
+library(pander)
+
+
+texreg::plotreg(model_list,
+                custom.model.names = c("Mental Health", "Depression", "Sleep"),
+                custom.coef.map = list(
+                 "(Intercept)" = "Intercept",
+                 "NDVI_MEAN" = "Mean NDVI",
+                 "greenspace_n" = "GS Area",
+                 "pct_white" = "% White",
+                 "pct_125k" = "% > $125k",
+                 "amens_per" = "Amenities",
+                 "trail_per_area" = "Trails",
+                 "rho" = "Rho"
+                ), reorder.coef = c(1, 8, 2, 3, 4, 5, 6, 7),
+                theme = theme_bw(),
+                type = "forest") %>% 
+  ggsave(filename = "fig/forest_reg.png",
+         width = 4.5, units = "in", dpi = 600)
+
