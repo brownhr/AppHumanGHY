4e7ba21475d98e05f0a01a17b58549e013b893f8
brownhr <brownhr314@gmail.com>
2022-06-10 14:41:47 -0400

    Update spatial_lag.R

--- R/spatial_lag.R
+++ R/spatial_lag.R
@@ -1,9 +1,25 @@
 library(spdep)
 library(spatialreg)
+library(broom)
+library(texreg)
 
 source("R/LISA.R")
 source("R/neighbors.R")
 
+
+mhealth_basic <- lm(  formula = MHLTH ~ NDVI_MEAN +
+                        greenspace_n +
+                        pct_white +
+                        pct_125k +
+                        amens_per +
+                        trail_per_area,
+                      data = zcta_main)
+
+lm_test <- lm.LMtests(mhealth_basic,
+                      listw = zcta_listw,
+                      zero.policy = TRUE,
+                      test = "all")
+lagrange_sum <- summary(lm_test)
 
 mhealth_lag <- lagsarlm(
   formula = MHLTH ~ NDVI_MEAN +
@@ -17,6 +33,19 @@ mhealth_lag <- lagsarlm(
   zero.policy = TRUE,
   na.action = na.omit
 ) 
+
+mhealth_err <- errorsarlm(
+  formula = MHLTH ~ NDVI_MEAN +
+    greenspace_n +
+    pct_white +
+    pct_125k +
+    amens_per +
+    trail_per_area,
+  data = zcta_main,
+  listw = zcta_listw,
+  zero.policy = TRUE,
+  na.action = na.omit
+)
 
 depress_lag <- lagsarlm(
   formula = DEPRESSION ~ NDVI_MEAN +
