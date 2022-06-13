ad76f22c82425020b5f98685b9cc4a7323452579
brownhr <brownhr314@gmail.com>
2022-06-13 18:11:22 -0400

    Create places_censustract.R

---
+++ R/places_censustract.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(tigris)
+library(sf)
+library(tmap)
+
+nc_censustract <- tigris::tracts(state = "NC",
+                            county = wnc_counties$COUNTYFP,
+                            cb = T)
+
+
+places_ct <-
+  read_csv(
+    "data/places_2021_censustract.csv",
+    col_types = cols(
+      Year = col_skip(),
+      StateDesc = col_skip(),
+      DataSource = col_skip(),
+      Measure = col_skip(),
+      Data_Value_Footnote_Symbol = col_skip(),
+      Data_Value_Footnote = col_skip(),
+      Low_Confidence_Limit = col_skip(),
+      High_Confidence_Limit = col_skip(),
+      Geolocation = col_skip(),
+      CategoryID = col_skip(),
+      DataValueTypeID = col_skip(),
+      Short_Question_Text = col_skip()
+    ),
+    trim_ws = FALSE
+  ) %>% 
+  filter(
+    StateAbbr == "NC",
+    MeasureId %in% c("MHLTH", "DEPRESSION", "SLEEP")
+  )
+
+nc_places <- left_join(nc_censustract, places_ct, by = c("GEOID" = "LocationName"))
+
+
+nc_places2 <- nc_places %>% 
+  select(
+    GEOID, NAMELSAD, CountyName, CountyFIPS,
+    Data_Value, TotalPopulation, MeasureId
+  )
+
+
+nc_places_wide <- nc_places2 %>% 
+  pivot_wider(names_from = MeasureId, values_from = Data_Value)
+
+
+nc_places_wide %>% 
+  qtm(fill = "MHLTH")
