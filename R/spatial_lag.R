f009810cf33f5c16544a3628e69f33965389f0d0
brownhr <brownhr314@gmail.com>
2022-06-14 11:19:32 -0400

    Update spatial_lag.R

--- R/spatial_lag.R
+++ R/spatial_lag.R
@@ -7,6 +7,38 @@ source("R/LISA.R")
 source("R/neighbors.R")
 
 
+mhealth_basic <- lm(
+  formula = MHLTH ~ NDVI_MEAN +
+    greenspace_n +
+    pct_white +
+    pct_125k +
+    amens_per +
+    trail_per_area,
+  data = zcta_main
+)
+depress_basic <- lm(
+  formula = DEPRESSION ~ NDVI_MEAN +
+    greenspace_n +
+    pct_white +
+    pct_125k +
+    amens_per +
+    trail_per_area,
+  data = zcta_main
+)
+sleep_basic <- lm(
+  formula = SLEEP ~ NDVI_MEAN +
+    greenspace_n +
+    pct_white +
+    pct_125k +
+    amens_per +
+    trail_per_area,
+  data = zcta_main
+)
+
+basic_list <- list("MHEALTH" = mhealth_basic,
+                   "DEPRESSION" =  depress_basic,
+                   "SLEEP" = sleep_basic) %>% 
+  write_rds("data/basic_list.Rds")
 
 # lm_test <- lm.LMtests(mhealth_basic,
 #   listw = zcta_listw,
@@ -19,8 +51,8 @@ mhealth_lag <- lagsarlm(
   formula = MHLTH ~ NDVI_MEAN +
     greenspace_n +
     pct_white +
-    # pct_125k +
-    # amens_per +
+    pct_125k +
+    amens_per +
     trail_per_area,
   data = zcta_main,
   listw = zcta_listw,
@@ -33,8 +65,8 @@ depress_lag <- lagsarlm(
   formula = DEPRESSION ~ NDVI_MEAN +
     greenspace_n +
     pct_white +
-    # pct_125k +
-    # amens_per +
+    pct_125k +
+    amens_per +
     trail_per_area,
   data = zcta_main,
   listw = zcta_listw,
@@ -46,8 +78,8 @@ sleep_lag <- lagsarlm(
   formula = SLEEP ~ NDVI_MEAN +
     greenspace_n +
     pct_white +
-    # pct_125k +
-    # amens_per +
+    pct_125k +
+    amens_per +
     trail_per_area,
   data = zcta_main,
   listw = zcta_listw,
